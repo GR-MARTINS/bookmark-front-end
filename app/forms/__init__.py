8fd47a8ba1dc8fedfe99f9b07cb9d6153467128e
Glayton Martins <gr.martins@outlook.com>
2022-05-10 20:55:36 -0300

    forms module updated: Register class created

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,1 +1,33 @@
+from wtforms import (
+    StringField,
+    EmailField,
+    PasswordField,
+    SubmitField,
+)
+from wtforms.validators import (
+    DataRequired,
+    Email,
+    EqualTo
+)
+from flask_wtf import FlaskForm
 
+
+class Register(FlaskForm):
+    username = StringField(
+        "Password", validators=[DataRequired()]
+    )
+
+    email = EmailField(
+        "Email", validators=[DataRequired(), Email()]
+    )
+
+    password = PasswordField(
+        "Password", validators=[DataRequired()]
+    )
+
+    confirm_password = PasswordField(
+        'Confirm Password',
+        validators=[DataRequired(), EqualTo('password')]
+    )
+
+    submit = SubmitField('Submit')
