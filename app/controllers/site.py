734b53afa46c77464f31d842cb9338d822a22132
Glayton Martins <gr.martins@outlook.com>
2022-05-14 17:35:40 -0300

    controllers module updated: CreateBookmarkForm class instantiated in creat_form object.

--- app/controllers/site.py
+++ app/controllers/site.py
@@ -1,19 +1,32 @@
 from flask import (
     Blueprint,
-    render_template
+    render_template,
 )
-from app.forms import Login, Register, Bookmark
+from app.forms import (
+    Login,
+    Register,
+    CreateBookmarkForm
+)
 
 site = Blueprint("site", __name__)
 
 
-@site.route('/')
+@site.route("/")
 def index():
-    form = Bookmark()
-    return render_template("home.html", form=form)
+    token = "a"
+    if token:
+        create_form = CreateBookmarkForm()
+        return render_template(
+            "index.html",
+            create_form=create_form,
+            token=token
+        )
+
+    else:
+        return render_template("index.html")
 
 
-@site.route('/login')
+@site.route("/login", methods=['GET', 'POST'])
 def login():
     form = Login()
     return render_template("login.html", form=form, title="Login")
